a09de2e1443de2197b0cf9cebda5a7b3786f6277
CinzadeNeve <ticlaudioalves@gmail.com>
2024-09-19 09:29:51 -0300

    fix: ajustes no livro

--- public/src/js/classes/Livro/Livro.ts
+++ public/src/js/classes/Livro/Livro.ts
@@ -2,6 +2,8 @@ import Pagina from "./Pagina";
 
 class Livro{
     private paginas:Pagina[] = [];
+    private page_view_left:number = 0;
+    private page_view_right:number = 1;
 
     public add_page(page:Pagina){
         this.paginas.push(page)
@@ -26,6 +28,27 @@ class Livro{
         this.paginas.forEach((pagina)=>{
             console.log(pagina);
         })
+    }
+
+
+    public get_page_view_left():number{
+        return this.page_view_left;
+    }
+
+    public get_page_view_right():number{
+        return this.page_view_right;
+    }
+
+
+    /** Seguir para a próxima página do livro */
+    public next_page_view(){
+        this.page_view_left = this.page_view_left + 2;
+        this.page_view_right = this.page_view_right + 2;
+    }
+
+    public prev_page_view(){
+        this.page_view_right = this.page_view_right - 2;
+        this.page_view_left = this.page_view_left - 2;
     }
     
 
--- public/src/js/main.ts
+++ public/src/js/main.ts
@@ -8,6 +8,7 @@ import Livro from "./classes/Livro/Livro";
 
 
 import Player from "./classes/sistema/Player";
+import { deck_starter_set_1 } from "./classes/sistema/DeckMontados";
 
 import {bd_card} from "./bd_cards";
 
@@ -19,66 +20,41 @@ function filtro_card(id:string){
 
     switch(card?.categoria){
         case 'Evento':
-            return new Evento(card.nome, card.id, '', card.raridade, card.descricao, card.texto_adicional, card.batalha_tipo?.ataque, card.batalha_tipo?.defesa);
+            return new Evento(card.nome, card.id, `./public/src/img/cards/${card.id}/${card.id}.webp`, card.raridade, card.descricao, card.texto_adicional, card.batalha_tipo?.ataque, card.batalha_tipo?.defesa);
         case 'Mágia':
-            return new Poder(card.nome, card.id, '', card.raridade, card.descricao, card.texto_adicional, card.atributo, card.poder, card.dano_livro, card.mamodo, card.batalha_tipo?.ataque, card.batalha_tipo?.defesa)
+            return new Poder(card.nome, card.id, `./public/src/img/cards/${card.id}/${card.id}.webp`, card.raridade, card.descricao, card.texto_adicional, card.atributo, card.poder, card.dano_livro, card.mamodo, card.batalha_tipo?.ataque, card.batalha_tipo?.defesa)
         case 'Mamodo':
-            return new Mamodo(card.nome, card.id, '', card.raridade, card.descricao, card.texto_adicional, card.parceiro, card.poder);
+            return new Mamodo(card.nome, card.id, `./public/src/img/cards/${card.id}/${card.id}.webp`, card.raridade, card.descricao, card.texto_adicional, card.parceiro, card.poder);
         case 'Parceiro':
-            return new Parceiro(card.nome, card.id, '', card.raridade, card.descricao, card.texto_adicional, card.parceiro_mamodo);
+            return new Parceiro(card.nome, card.id, `./public/src/img/cards/${card.id}/${card.id}.webp`, card.raridade, card.descricao, card.texto_adicional, card.parceiro_mamodo);
         default:
             console.log("Não indentificado");
             return undefined;
     }
 }
 
-const Livro1 = new Livro();
+function construir_livro(array_paginas:string[], livro:Livro){
+    for(let [index, value] of array_paginas.entries()){
 
-
-const array_paginas:string[] = [
-    "M-001",
-    "S-025",
-    "S-001",
-    "P-001",
-    "S-001",
-    "M-014",
-    "S-027",
-    "P-009",
-    "S-022",
-    "S-026",
-    "S-001",
-    "S-003",
-    "E-004",
-    "M-012",
-    "S-003",
-    "P-008",
-    "S-020",
-    "S-004",
-    "E-009",
-    "E-003",
-    "S-023",
-    "E-008",
-    "S-022",
-    "E-001",
-    "S-023",
-    "E-010",
-    "S-002",
-    "E-011",
-    "S-021",
-    "E-002",
-    "S-024",
-    "S-005"
-]
-
-
-for(let [index, value] of array_paginas.entries()){
-
-    const card:Evento | Poder | Mamodo | Parceiro | undefined = filtro_card(value);
-    if(card){
-        const pagina:Pagina = new Pagina(card,index);
-        Livro1.add_page(pagina);
+        const card:Evento | Poder | Mamodo | Parceiro | undefined = filtro_card(value);
+        if(card){
+            const pagina:Pagina = new Pagina(card,index);
+            livro.add_page(pagina);
+        }
     }
 }
+
+
+/** Declaração dos livros */
+const Livro1 = new Livro();
+const Livro2 = new Livro();
+
+/** Lista de ID das cartas do deck Starter Set 1 */
+const array_paginas:string[] = deck_starter_set_1();
+
+/** Monstando o livro */
+construir_livro(array_paginas, Livro1);
+
 
 Livro1.print_livro();
 
